1dee8ad110dcbcb462bd8744c7126b3ef074d18a
Leticia <leticiancalves1@gmail.com>
2021-11-25 00:02:32 -0300

    contents

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import FeedModal from '../User/FeedModal';
+import FeedPhotos from '../User/FeedPhotos';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  return (
+    <div>
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
 };
 
 export default Feed;
